f36f677c79ca7aa49ce720a518e136234a8b120a
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-01-23 12:17:28 +0100

    fix issue with SockJS

--- frontend/src/app/web-chat/web-chat.service.ts
+++ frontend/src/app/web-chat/web-chat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CompatClient, IFrame, IMessage, Stomp } from '@stomp/stompjs';
-import { SockJS } from 'sockjs-client';
+import * as  SockJS from 'sockjs-client';
 
 @Injectable({
   providedIn: 'root'
